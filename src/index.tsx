72328ebb3556cf28f806f5324395c66e3022fa4d
Anton Ivanov <a.ivanov@rusiem.com>
2021-11-01 11:09:28 +0300

    Base app setup

---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <React.StrictMode>
+    App bootstrap
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
---
+++ webpack.config.js
@@ -0,0 +1,64 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.tsx",
+  output: {
+    filename: "[name].[fullhash].js",
+    path: path.resolve(__dirname, "public"),
+    publicPath: "/"
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "public", "index.html"),
+    })
+  ],
+  resolve: {
+    modules: [__dirname, "src", "node_modules"],
+    extensions: ["*", ".js", ".jsx", ".tsx", ".ts"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        enforce: "pre",
+        use: ["source-map-loader"],
+      },
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: require.resolve("babel-loader"),
+      },
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              "@babel/preset-env",
+              "@babel/preset-react",
+              "@babel/preset-typescript",
+            ],
+            plugins: [
+              "@babel/plugin-transform-runtime",
+            ]
+          },
+        },
+      },
+      {
+        test: /\.s[ac]ss$/,
+        include: /src/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.png|svg|jpg|gif$/,
+        use: ["file-loader"],
+      },
+    ],
+  },
+  devServer: {
+    historyApiFallback: true,
+  },
+};
