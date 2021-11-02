5022caf106a9a199c74b0eabd08b0716e0626a92
Anton Ivanov <a.ivanov@rusiem.com>
2021-11-02 16:12:33 +0300

    Added logo

--- src/layouts/MainLayout/components/SidebarPane/index.tsx
+++ src/layouts/MainLayout/components/SidebarPane/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 
+import logo from 'assets/images/rusiem_black.svg';
 import './styles.scss';
 
 const SidebarPane: React.FC = () => (
   <div className="sidebar-pane">
     <div className="sidebar-pane__brand">
-      <a href="/">Brand Logo</a>
+      <a href="/" title="Перейти на главную">
+        <img src={logo} alt="RuSIEM Система управления сайтами" width="150" />
+      </a>
     </div>
     <div className="sidebar-pane__nav">
       Navigation
