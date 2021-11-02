709448b15574bfc6abed54b1bd985dcf7db1d864
Anton Ivanov <a.ivanov@rusiem.com>
2021-11-02 11:03:52 +0300

    Added FormView support to global application store

--- src/store/context.ts
+++ src/store/context.ts
@@ -1,9 +1,15 @@
 import React from 'react';
 
+import { INITIAL_STATE as FORM_VIEW_STATE } from 'views/FormView/store/reducer';
+
 import { TReducersContext } from './types';
 
-export const DEFAULT_VALUE = {
-  /**/
+export const DEFAULT_VALUE: TReducersContext = {
+  formView: {
+    state: FORM_VIEW_STATE,
+    update: () => false,
+    reset: () => false,
+  },
 };
 
 const Context = React.createContext<TReducersContext>(DEFAULT_VALUE);
--- src/store/types.ts
+++ src/store/types.ts
@@ -1,5 +1,7 @@
+import type { TUseReducer } from 'views/FormView/hooks/useReducer';
+
 export type TReducers = {
-  /**/
+  formView: TUseReducer
 };
 
 export type TReducersContext = TReducers;
