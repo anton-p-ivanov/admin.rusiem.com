d6e4bdda5f65737bfd97c4589abb586ad2319d24
Anton Ivanov <a.ivanov@rusiem.com>
2021-11-08 10:03:46 +0300

    Webpack .env support update

--- webpack.config.js
+++ webpack.config.js
@@ -3,70 +3,74 @@ const Dotenv = require("dotenv-webpack");
 const path = require("path");
 const fs = require("fs");
 
-const envLocal = path.resolve(__dirname, ".env.local");
-const env = path.resolve(__dirname, ".env");
+module.exports = (env, options) => {
+  const mode = options.mode || 'development';
+  const dotenv = [`.env.${mode}.local`, `.env.${mode}`, ".env.local", ".env"]
+      .filter((filename) => fs.existsSync(path.resolve(__dirname, filename)))
+      .shift();
 
-module.exports = {
-  mode: "development",
-  entry: "./src/index.tsx",
-  output: {
-    filename: "[name].[fullhash].js",
-    path: path.resolve(__dirname, "public"),
-    publicPath: "/"
-  },
-  plugins: [
-    new Dotenv({
-      path: fs.existsSync(envLocal) ? envLocal : env
-    }),
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "public", "index.html"),
-    })
-  ],
-  resolve: {
-    modules: [__dirname, "src", "node_modules"],
-    extensions: ["*", ".js", ".jsx", ".tsx", ".ts"],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        enforce: "pre",
-        use: ["source-map-loader"],
-      },
-      {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        loader: require.resolve("babel-loader"),
-      },
-      {
-        test: /\.tsx?$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: [
-              "@babel/preset-env",
-              "@babel/preset-react",
-              "@babel/preset-typescript",
-            ],
-            plugins: [
-              "@babel/plugin-transform-runtime",
-            ]
+  return ({
+    mode,
+    entry: "./src/index.tsx",
+    output: {
+      filename: "[name].[fullhash].js",
+      path: path.resolve(__dirname, "public"),
+      publicPath: "/"
+    },
+    plugins: [
+      new Dotenv({
+        path: dotenv
+      }),
+      new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, "public", "index.html"),
+      })
+    ],
+    resolve: {
+      modules: [__dirname, "src", "node_modules"],
+      extensions: ["*", ".js", ".jsx", ".tsx", ".ts"],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          enforce: "pre",
+          use: ["source-map-loader"],
+        },
+        {
+          test: /\.jsx?$/,
+          exclude: /node_modules/,
+          loader: require.resolve("babel-loader"),
+        },
+        {
+          test: /\.tsx?$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: [
+                "@babel/preset-env",
+                "@babel/preset-react",
+                "@babel/preset-typescript",
+              ],
+              plugins: [
+                "@babel/plugin-transform-runtime",
+              ]
+            },
           },
         },
-      },
-      {
-        test: /\.s[ac]ss$/,
-        include: /src/,
-        use: ["style-loader", "css-loader", "sass-loader"],
-      },
-      {
-        test: /\.png|svg|jpg|gif$/,
-        use: ["file-loader"],
-      },
-    ],
-  },
-  devServer: {
-    historyApiFallback: true,
-  },
+        {
+          test: /\.s[ac]ss$/,
+          include: /src/,
+          use: ["style-loader", "css-loader", "sass-loader"],
+        },
+        {
+          test: /\.png|svg|jpg|gif$/,
+          use: ["file-loader"],
+        },
+      ],
+    },
+    devServer: {
+      historyApiFallback: true,
+    },
+  });
 };
