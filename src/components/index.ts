ae5da9c0f851feb5f3a70bb361068cea3cbde9ed
Anton Ivanov <a.ivanov@rusiem.com>
2021-11-18 15:15:36 +0300

    Added Modal and Tabs components

--- src/components/index.ts
+++ src/components/index.ts
@@ -5,9 +5,11 @@ import DataToolbar from './DataToolbar';
 import DropDown from './DropDown';
 import Form from './Form';
 import Icon from './Icon';
+import Modal from './Modal';
 import Nav from './Nav';
 import Overlay from './Overlay';
 import Pagination from './Pagination';
+import Tabs from './Tabs';
 
 export {
   Button,
@@ -17,7 +19,9 @@ export {
   DropDown,
   Form,
   Icon,
+  Modal,
   Nav,
   Overlay,
   Pagination,
+  Tabs,
 };
