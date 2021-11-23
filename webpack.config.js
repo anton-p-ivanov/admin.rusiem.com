816178f23ed02f8df742fb8da6565ee65a90be91
Anton Ivanov <a.ivanov@rusiem.com>
2021-11-23 12:27:00 +0300

    test

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   entry: "./src/index.tsx",
   output: {
     filename: "[name].[fullhash].js",
-    path: path.resolve(__dirname, "public/dist"),
+    path: path.resolve(__dirname, "public"),
     publicPath: "/"
   },
   plugins: [
