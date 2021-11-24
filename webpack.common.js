d94682198e47359480c1b6ad429fdbcf3b09cdf5
Anton Ivanov <a.ivanov@rusiem.com>
2021-11-24 14:31:00 +0300

    Webpack update

---
+++ webpack.common.js
@@ -0,0 +1,62 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: {
+    app: './src/index.tsx'
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'public/build'),
+    publicPath: '/',
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Production',
+      template: path.resolve(__dirname, 'public', 'index.html'),
+      favicon: path.resolve(__dirname, 'public', 'favicon.ico'),
+    })
+  ],
+  resolve: {
+    modules: [__dirname, "src", "node_modules"],
+    extensions: ["*", ".js", ".jsx", ".tsx", ".ts"],
+    alias: {
+      '@news': path.resolve(__dirname, "src/modules/content/modules/news")
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              "@babel/preset-env",
+              "@babel/preset-react",
+              "@babel/preset-typescript",
+            ],
+            plugins: [
+              "@babel/plugin-transform-runtime",
+            ]
+          },
+        },
+      },
+      {
+        test: /\.s[ac]ss$/,
+        include: /src/,
+        use: [
+          "style-loader",
+          "css-loader",
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.png|svg|jpg|gif$/,
+        use: ["file-loader"],
+      },
+    ],
+  },
+};
