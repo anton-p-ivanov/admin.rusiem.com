228e91c0e2a7a244051ce0c8fca2b5f5d6375b38
Anton Ivanov <a.ivanov@rusiem.com>
2021-11-30 15:48:51 +0300

    Vacancies form

--- src/modules/content/modules/vacancies/modules/vacancies/pages/EditPage.tsx
+++ src/modules/content/modules/vacancies/modules/vacancies/pages/EditPage.tsx
@@ -2,11 +2,16 @@ import React from 'react';
 
 import PageView from 'views/PageView';
 
+import VacanciesForm from '../views/VacanciesForm';
+
 const EditPage: React.FC = () => {
   const title = 'Контент :: Вакансии';
 
   return (
-    <PageView title={title} />
+    <PageView title={title}>
+      <h2>Изменение элемента</h2>
+      <VacanciesForm />
+    </PageView>
   );
 };
 
