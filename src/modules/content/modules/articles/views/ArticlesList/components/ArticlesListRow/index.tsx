de7e04894870f653e5d654419f32facc77483fa6
Anton Ivanov <a.ivanov@rusiem.com>
2021-11-30 15:53:55 +0300

    Refactoring props

--- src/modules/content/modules/articles/views/ArticlesList/components/ArticlesListRow/index.tsx
+++ src/modules/content/modules/articles/views/ArticlesList/components/ArticlesListRow/index.tsx
@@ -6,34 +6,30 @@ import ArticlesListContext from '../ArticlesListContext';
 
 import type { TArticlesListRowProps } from './types';
 
-const ArticlesListRow: React.FC<TArticlesListRowProps> = (props) => {
-  const { data } = props;
-
-  return (
-    <>
-      <td className="data-table__cell data-table__cell--title">
-        {data.title}
-      </td>
-      <td className="data-table__cell data-table__cell--publishedAt">
-        <DataFormat format="date" value={data.publishedAt} />
-      </td>
-      <td className="data-table__cell data-table__cell--locale">
-        {data.locale.toUpperCase()}
-      </td>
-      <td className="data-table__cell data-table__cell--isPublished">
-        {data.isPublished && <Icon name="check" variant="success" />}
-        {!data.isPublished && <Icon name="x" variant="muted" />}
-      </td>
-      <td className="data-table__cell data-table__cell--updatedAt">
-        {data.workflow
-          ? <DataFormat format="datetime" value={data.workflow.updatedAt} />
-          : <em>Нет данных</em>}
-      </td>
-      <td className="data-table__cell data-table__cell--context">
-        <ArticlesListContext data={data} />
-      </td>
-    </>
-  );
-};
+const ArticlesListRow: React.FC<TArticlesListRowProps> = ({ data }) => (
+  <>
+    <td className="data-table__cell data-table__cell--title">
+      {data.title}
+    </td>
+    <td className="data-table__cell data-table__cell--publishedAt">
+      <DataFormat format="date" value={data.publishedAt} />
+    </td>
+    <td className="data-table__cell data-table__cell--locale">
+      {data.locale.toUpperCase()}
+    </td>
+    <td className="data-table__cell data-table__cell--isPublished">
+      {data.isPublished && <Icon name="check" variant="success" />}
+      {!data.isPublished && <Icon name="x" variant="muted" />}
+    </td>
+    <td className="data-table__cell data-table__cell--updatedAt">
+      {data.workflow
+        ? <DataFormat format="datetime" value={data.workflow.updatedAt} />
+        : <em>Нет данных</em>}
+    </td>
+    <td className="data-table__cell data-table__cell--context">
+      <ArticlesListContext data={data} />
+    </td>
+  </>
+);
 
 export default ArticlesListRow;
--- src/modules/content/modules/news/views/NewsList/components/NewsListRow/index.tsx
+++ src/modules/content/modules/news/views/NewsList/components/NewsListRow/index.tsx
@@ -6,34 +6,30 @@ import NewsListContext from '../NewsListContext';
 
 import type { TNewsListRowProps } from './types';
 
-const NewsListRow: React.FC<TNewsListRowProps> = (props) => {
-  const { data } = props;
-
-  return (
-    <>
-      <td className="data-table__cell data-table__cell--title">
-        {data.title}
-      </td>
-      <td className="data-table__cell data-table__cell--publishedAt">
-        <DataFormat format="date" value={data.publishedAt} />
-      </td>
-      <td className="data-table__cell data-table__cell--locale">
-        {data.locale.toUpperCase()}
-      </td>
-      <td className="data-table__cell data-table__cell--isPublished">
-        {data.isPublished && <Icon name="check" variant="success" />}
-        {!data.isPublished && <Icon name="x" variant="muted" />}
-      </td>
-      <td className="data-table__cell data-table__cell--updatedAt">
-        {data.workflow
-          ? <DataFormat format="datetime" value={data.workflow.updatedAt} />
-          : <em>Нет данных</em>}
-      </td>
-      <td className="data-table__cell data-table__cell--context">
-        <NewsListContext data={data} />
-      </td>
-    </>
-  );
-};
+const NewsListRow: React.FC<TNewsListRowProps> = ({ data }) => (
+  <>
+    <td className="data-table__cell data-table__cell--title">
+      {data.title}
+    </td>
+    <td className="data-table__cell data-table__cell--publishedAt">
+      <DataFormat format="date" value={data.publishedAt} />
+    </td>
+    <td className="data-table__cell data-table__cell--locale">
+      {data.locale.toUpperCase()}
+    </td>
+    <td className="data-table__cell data-table__cell--isPublished">
+      {data.isPublished && <Icon name="check" variant="success" />}
+      {!data.isPublished && <Icon name="x" variant="muted" />}
+    </td>
+    <td className="data-table__cell data-table__cell--updatedAt">
+      {data.workflow
+        ? <DataFormat format="datetime" value={data.workflow.updatedAt} />
+        : <em>Нет данных</em>}
+    </td>
+    <td className="data-table__cell data-table__cell--context">
+      <NewsListContext data={data} />
+    </td>
+  </>
+);
 
 export default NewsListRow;
