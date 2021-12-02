d579f6c82524469c937166c824d0ad8368dc74c6
Anton Ivanov <a.ivanov@rusiem.com>
2021-12-02 16:18:13 +0300

    ListView context menu can be customized with child nodes

--- src/views/ListView/components/ListViewContext/index.tsx
+++ src/views/ListView/components/ListViewContext/index.tsx
@@ -3,10 +3,12 @@ import React, { useContext } from 'react';
 import { DropDown, Icon } from 'components';
 import { Context as ModalContext } from 'wrappers/withModal';
 
+import Context from './context';
+
 import type { TListViewContextProps } from './types';
 
 const ListViewContext: React.FC<TListViewContextProps> = (props) => {
-  const { data, baseUrl } = props;
+  const { data, baseUrl, children } = props;
   const { uuid = 'unknown', title } = data;
   const { toggle } = useContext(ModalContext);
   const toggleDropdown = <Icon name="more-vertical" title="Выберите действие" />;
@@ -19,12 +21,20 @@ const ListViewContext: React.FC<TListViewContextProps> = (props) => {
   );
 
   return (
-    <DropDown toggle={toggleDropdown}>
-      <DropDown.Link icon="edit" route={`${baseUrl}/${uuid}/edit`}>Изменить</DropDown.Link>
-      <DropDown.Link icon="copy" route={`${baseUrl}/${uuid}/copy`}>Копировать</DropDown.Link>
-      <DropDown.Divider />
-      <DropDown.Button icon="trash-2" onClick={removeHandler}>Удалить</DropDown.Button>
-    </DropDown>
+    <Context.Provider value={{ remove: removeHandler }}>
+      <DropDown toggle={toggleDropdown}>
+        <Context.Consumer>
+          {children || ((context) => (
+            <>
+              <DropDown.Link icon="edit" route={`${baseUrl}/${uuid}/edit`}>Изменить</DropDown.Link>
+              <DropDown.Link icon="copy" route={`${baseUrl}/${uuid}/copy`}>Копировать</DropDown.Link>
+              <DropDown.Divider />
+              <DropDown.Button icon="trash-2" onClick={context.remove}>Удалить</DropDown.Button>
+            </>
+          ))}
+        </Context.Consumer>
+      </DropDown>
+    </Context.Provider>
   );
 };
 
--- src/views/ListView/components/ListViewContext/types.ts
+++ src/views/ListView/components/ListViewContext/types.ts
@@ -1,6 +1,13 @@
-import { TDataEntity } from 'config/types';
+import React from 'react';
+
+import type { TDataEntity } from 'config/types';
 
 export type TListViewContextProps = {
   data: TDataEntity;
   baseUrl: string;
+  children?: (value: TListViewContextContext) => React.ReactNode;
+};
+
+export type TListViewContextContext = {
+  remove: () => void;
 };
