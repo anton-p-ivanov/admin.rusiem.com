bcb17843cc17fca95f075e22764e1a2bc0888398
Anton Ivanov <a.ivanov@rusiem.com>
2021-12-09 14:13:25 +0300

    Form can be submitted via `submit` or `apply` buttons

--- src/views/EditView/index.tsx
+++ src/views/EditView/index.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 
 import { Button, Form } from 'components';
 import FormView, { withStore, Context } from 'views/FormView';
@@ -9,25 +9,35 @@ import type { TEditViewProps } from './types';
 
 import './styles.scss';
 
-const EditView: React.FC<TEditViewProps> = (props) => {
-  const {
-    fields, defaults, endpoints, children,
-  } = props;
+const EditView: React.FC<TEditViewProps> = ({ children, ...props }) => {
+  const [action, setAction] = useState<string>('submit');
+  const { fields, defaults, endpoints } = props;
+  const navigate = useNavigate();
+
+  const onSubmitSucceed = () => {
+    if (action === 'submit') {
+      navigate(endpoints?.success || '/');
+    }
+  };
 
   return (
-    <FormView endpoints={endpoints} fields={fields} defaults={defaults}>
+    <FormView
+      endpoints={endpoints}
+      fields={fields}
+      defaults={defaults}
+      onSubmitSucceed={onSubmitSucceed}
+    >
       <div className="grid-view">
         <Context.Consumer>
           {children}
         </Context.Consumer>
       </div>
       <Form.Actions>
-        <Button isSubmit variant="primary">
-          Сохранить
-        </Button>
-        {endpoints && (
-          <Link to={endpoints.success || '/'} className="btn btn--default">Отменить</Link>
-        )}
+        <div>
+          <Button isSubmit variant="primary" onClick={() => setAction('submit')}>Сохранить</Button>
+          <Button isSubmit variant="default" onClick={() => setAction('apply')}>Применить</Button>
+        </div>
+        <Link to={endpoints?.success || '/'} className="btn btn--default">Отменить</Link>
       </Form.Actions>
       <WorkflowView />
     </FormView>
